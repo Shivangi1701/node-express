35ae30d8238c629ff1e94c1ea979fe488bd7d852
Shivangi1701 <shivangi170102@gmail.com>
2023-03-20 12:57:18 +0530

    first commit

---
+++ 06-mind-grenade.js
@@ -0,0 +1,10 @@
+const num1 = 7;
+const num2 = 10;
+
+function addValues(){
+    console.log(`the sum is: ${num1 + num2}`)
+}
+
+addValues();
+// if there is a function inside a module 
+// it will work even if we don't invoke it or assign it to any variable
